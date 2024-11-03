5115f84f72cdf61d89246ea50df3afab5dd3a834
dev-mabdulrehman <dev.mabdulrehman@gmail.com>
2024-11-03 20:18:26 +0000

    Edit & Delete button added

--- src/components/CustomCheckButton.tsx
+++ src/components/CustomCheckButton.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 const CustomCheckButton = ({ id, isChecked, handleCheckChange }: Props) => {
     return (
-        <label role='checkbox' className={`w-6 h-6 cursor-pointer rounded-full flex items-center justify-center border-2 border-black/20 ${isChecked ? 'bg-green-200' : ''}`}>
+        <label role='checkbox' className={`w-6 flex-shrink-0 h-6 cursor-pointer rounded-full flex items-center justify-center border-2 border-black/20 ${isChecked ? 'bg-green-200' : ''}`}>
             {isChecked &&
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="size-4">
                     <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
